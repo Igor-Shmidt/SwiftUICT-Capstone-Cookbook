ea66119e1da2d6703494a1d8ce9f01a577a3ea6e
Igor Shmidt <Igore.Schmidt@icloud.com>
2026-08-11 14:32:51 +0200

    Recipe Local Repository added
    Added `RecipeRepositoryTests`conering Recipes fetching
    Added `RecipeRepository` ptotocol for Dependency Injection
    Added `LocalRecipeRepository` conforming `RecipeRepository` using base model `Recipes`
    Improved `AppDIContainer` adding `recipeRepository`
    Removed .gitkeep since there content added

---
+++ CookbookTests/RecipeRepositoryTests.swift
@@ -0,0 +1,44 @@
+//
+//  RecipeRepositoryTests.swift
+//  Cookbook
+//
+//  Created by Igor Shmidt on 10.08.2026.
+//
+
+
+import Testing
+@testable import SwiftUICT_Capstone_Cookbook // Use your actual target name here
+
+/// Test suite to verify the behavior of the RecipeRepository.
+@Suite("Recipe Repository Tests")
+struct RecipeRepositoryTests {
+    var repository: RecipeRepository
+    var mockDataSource: Recipes
+
+    @MainActor
+    init() async {
+        mockDataSource = Recipes()
+        AppDIContainer.shared = AppDIContainer(recipes: mockDataSource)
+        repository = AppDIContainer.shared.recipeRepository
+    }
+}
+
+extension RecipeRepositoryTests {
+    @Test("Repository successfully fetches all preloaded recipes")
+    func testFetchRecipes() async throws {
+        // Given: Setup the data source and the repository done by init
+
+        // When: Fetch the recipes asynchronously
+        let fetchedRecipes = try await repository.fetchRecipes()
+        
+        // Then: Verify we got the expected data
+        let expectedCount = await mockDataSource.table.count
+        #expect(fetchedRecipes.count == expectedCount, "Expected exactly \(expectedCount) preloaded recipes.")
+
+        // Check if the first recipe matches our preload data
+        let (firstRecipeName, expected) = await (
+            fetchedRecipes.first?.name, mockDataSource.table.first?.name
+        )
+        #expect(firstRecipeName == expected, "First recipe should be '\(expected)'.")
+    }
+}
